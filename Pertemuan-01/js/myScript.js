fb3361c8fcd2c3f4f70e2598550aedf9799f9267
akramF17 <145886383+akramF17@users.noreply.github.com>
2024-09-03 09:47:25 +0700

    Pertemuan 1

---
+++ Pertemuan-01/js/myScript.js
@@ -0,0 +1,8 @@
+/**
+ *  Nama File   : myScript.js
+ *  NIM/Nama    : 24060122120033/Zikry Alfahri Akram
+ */
+
+function myFunction(){
+    document.getElementById("paragraf").innerHTML = "Hypertext Markup Language";
+}
